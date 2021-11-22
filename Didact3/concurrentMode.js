0f0503dc33567942751baed6b87899fc0093cc85
IHFB FE team <kimjw@IHFBui-MacBookPro.local>
2021-11-22 23:33:33 +0900

    Didact3 - concurrentMode

--- Didact3/concurrentMode.js
+++ Didact3/concurrentMode.js
@@ -1,4 +1,6 @@
 //requestIdleCallback(callback) => 메인 스레드가 대기 상태일 때 브라우저가 callback 실행
+//deadline.timeRemaining(): returns the estimated number of milliseconds remaining in the current idle period.
+//https://developer.mozilla.org/en-US/docs/Web/API/IdleDeadline/timeRemaining
 
 let nextUnitOfWork = null
 ​
