6e46f8c6d5bc18aee4bab6d4b9e085e6f0cf0903
IHFB FE team <kimjw@IHFBui-MacBookPro.local>
2021-11-22 23:28:53 +0900

    Didact3 - concurrentMode

---
+++ Didact3/concurrentMode.js
@@ -0,0 +1,18 @@
+//requestIdleCallback(callback) => 메인 스레드가 대기 상태일 때 브라우저가 callback 실행
+
+let nextUnitOfWork = null
+​
+function workLoop(deadline) {
+  let shouldYield = false
+  while (nextUnitOfWork && !shouldYield) {
+    nextUnitOfWork = performUnitOfWork(nextUnitOfWork)
+    shouldYield = deadline.timeRemaining() < 1
+  }
+  requestIdleCallback(workLoop)
+}
+​
+requestIdleCallback(workLoop)
+​
+function performUnitOfWork(nextUnitOfWork) {
+  // TODO
+}
